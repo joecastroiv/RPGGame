915a3295b8c42a3dbd275228cf5c860c1d50602d
joecastroiv <joecastroiv@hotmail.com>
2025-04-10 22:39:10 -0400

    first commit moving previous project to repo for this seng350 assignment

---
+++ Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    enum CharacterClass
+    {
+        Warrior,
+        Mage,
+        Rogue
+    }
+
+    internal class Character
+    {
+        public string Name { get; set; }
+        public CharacterClass Class { get; set; }
+        public int Health { get; set; }
+        public List<string> Inventory { get; set; }
+
+        public Character(string name, CharacterClass characterClass)
+        {
+            Name = name;
+            Class = characterClass;
+            Health = 100;
+            Inventory = new List<string>();
+        }
+    }
+
+
+
+
+}
